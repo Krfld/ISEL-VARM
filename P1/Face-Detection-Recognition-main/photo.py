535ba79114540ef1f51269becb588002dec0873e
Krfld <rodrigofldias@gmail.com>
2022-04-30 17:46:34 +0100

    .

--- P1/Face-Detection-Recognition-main/photo.py
+++ P1/Face-Detection-Recognition-main/photo.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from mtcnn import MTCNN
+# from mtcnn import MTCNN
 
 # pip install opencv-python
 # pip install mtcnn
@@ -14,7 +14,7 @@ def classifier(classifier, image):
     if (classifier == 'haar_cascade'):
         # Find faces:
         faces = cv2.CascadeClassifier(
-            'cascades/data/haarcascade_eye.xml').detectMultiScale(image)
+            'cascades/data/haarcascade_frontalface_alt2.xml').detectMultiScale(image)
         # For each face found:
         for result in faces:
             x, y, w, h = result
@@ -23,7 +23,7 @@ def classifier(classifier, image):
             cv2.rectangle(image, (x, y), (x1, y1), (0, 0, 255), 2)
     elif (classifier == 'mtcnn'):
         # Find faces:
-        faces = MTCNN().detect_faces(image)
+        # faces = MTCNN().detect_faces(image)
         # For each face found:
         for result in faces:
             x, y, w, h = result['box']
